8259fb44944b9a746d10f4b1c4ef759d7a3904e2
Vanda Ponikarova <ponikarova@gmail.com>
2020-02-22 17:48:04 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!(Array.isArray(array)) || array.length === 0) {
+        return 0;
+    }
+    
+    let minNum = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minNum) {
+            minNum = array[i];
+        }
+    }
+
+    return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!(Array.isArray(array)) || array.length === 0) {
+        return 0;
+    }
+
+    let maxNum = array[0];
+
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxNum) {
+            maxNum = array[i];
+        }
+    }
+
+    return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!(Array.isArray(array)) || array.length === 0) {
+        return 0;
+    }
+
+    let sum = array.reduce( (prev, cur) => {
+        return (prev + cur);
+    });
+
+    return sum / array.length;
 }
